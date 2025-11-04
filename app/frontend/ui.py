38a6957419b00a93889065d2cb22da6dc2d4656c
Naman Chhaparia <chhaparianaman@gmail.com>
2025-11-05 05:23:36 +0530

    Added AWS Fargate Deployment

--- app/frontend/ui.py
+++ app/frontend/ui.py
@@ -9,7 +9,7 @@ from app.common.custom_exception import CustomException
 logger = get_logger(__name__)
 
 st.set_page_config(page_title="Multi AI Agent", layout="centered")
-st.title("Multi AI Agent using Groq and Tavily New")
+st.title("Multi AI Agent using Groq and Tavily Updated")
 
 system_prompt = st.text_area("Define your AI Agent:", height=70, value="You are helpful.")
 selected_model = st.selectbox("Select your AI Model: ", settings.ALLOWED_MODEL_NAMES)
